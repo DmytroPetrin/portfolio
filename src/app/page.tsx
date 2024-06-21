b73e91ead50eee78721ccf5a7ed1564054471a0b
Dmytro Petrin <chichin91@hotmail.com>
2024-06-22 00:52:29 +0200

    created main component and cv.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import {Box} from '@mui/material'
 import styles from "./page.module.css"
+import {basics} from './cv.json'
+import Hero from './components/hero'
+import About from './components/about'
+import Experience from  './components/experience'
+import Education from './components/education'
+import Projects from './components/projects'
+import Skills from './components/skills'
 
+const {name} = basics
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <Box> hola mundo</Box>
+     <Hero/>
+     <About/>
+     <Experience/>
+     <Education/>
+     <Projects/>
+     <Skills/>
     </main>
   );
 }
