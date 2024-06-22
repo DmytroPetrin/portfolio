14586e5c744a3acb458116feeaafd611abb52aaa
Dmytro Petrin <chichin91@hotmail.com>
2024-06-22 19:19:06 +0200

    add photo

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,10 +1,18 @@
 import { Box } from "@mui/material"
+import photo from "../assets/foto_2024.png"
 
 export default function Hero(){
 
     return(
         <>
-        <Box>hero</Box>
+        <Box>
+            <Box>
+            
+            </Box>
+            <Box>
+                <img src={require("../assets/foto_2024.png").default} alt="photo" />
+            </Box>
+        </Box>
         </>
     )
 }
